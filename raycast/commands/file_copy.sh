5695935d39ad5730e6519881b3284b1a13049555
tianli <zengtianli1@gmail.com>
2026-04-16 17:13:15 +0800

    sync: 2026-04-16 17:13

--- raycast/commands/file_copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# @raycast.schemaVersion 1
-# @raycast.title file-copy
-# @raycast.mode fullOutput
-# @raycast.icon 📋
-# @raycast.packageName Files
-# @raycast.description Copy selected file's filename (and optionally content) to clipboard
-# @raycast.argument1 { "type": "dropdown", "placeholder": "Mode", "data": [{"title": "Filename Only", "value": "name"}, {"title": "Name + Content", "value": "content"}] }
-source "$(dirname "$(realpath "$0")")/../lib/run_python.sh" && run_python "file/file_copy.py" "$@"
--- raycast/commands/folder_paste.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# @raycast.schemaVersion 1
-# @raycast.title folder_paste
-# @raycast.mode fullOutput
-# @raycast.icon 📋
-# @raycast.packageName Custom
-# @raycast.description 将剪贴板中的文件粘贴到 Finder 当前目录
-source "$(dirname "$(realpath "$0")")/../lib/run_python.sh" && run_shell "file/folder_paste.sh" "$@"
